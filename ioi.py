5f912e7ba53f68b63d1cbb39c01de72b5c15e5f8
usohrab18-ux <usohrab18@gmail.com>
2025-11-17 06:52:10 +0430

    Create ioi.py

---
+++ ioi.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(_name_)
+
+@app.route('/')
+def home():
+    return "Bot is running successfully!"
+
+if _name_ == '_main_':
+    app.run(host='0.0.0.0', port=10000)￼Enter
